151723979421105f38fe4b7fe5841ff5ef4ec8fa
benbaessler <mail@benbaessler.com>
2023-11-17 22:27:40 +0300

    Import external contracts + add checks

--- contracts/src/Bloom.sol
+++ contracts/src/Bloom.sol
@@ -1,52 +1,86 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IERC20} from './interfaces/IERC20.sol';
+import {IERC20} from "./interfaces/IERC20.sol";
+import {IERC721} from "./interfaces/IERC721.sol";
 
 contract BloomCore {
-	// TODO: Potentially add transactionExecutor
+
+	address public OPEN_ACTION_CONTRACT;
+
+	constructor(address _openActionContract) {
+		OPEN_ACTION_CONTRACT = _openActionContract;
+	}
+
+    // TODO: Potentially add transactionExecutor
     struct Promotion {
+        address transactionExecutor;
         uint256 profileId;
         uint256 pubId;
         uint256 budget;
-		address token;
+        address token;
         uint256 rewardPerMirror;
-		uint256 minFollowers;
+        uint256 minFollowers;
     }
+
+	modifier onlyOpenAction() {
+		require(msg.sender == OPEN_ACTION_CONTRACT);
+		_;
+	}
 
     mapping(uint256 profileId => mapping(uint256 pubId => Promotion))
         public promotions;
 
-	function createPromotion(
-		uint256 profileId,
-		uint256 pubId,
-		uint256 budget,
-		address token,
-		uint256 rewardPerMirror,
-		uint256 minFollowers
-	) external {
+    address public constant lensHubProxyContract =
+        0xC1E77eE73403B8a7478884915aA599932A677870;
 
-		// TODO: add checks
+    function createPromotion(
+        address transactionExecutor,
+        uint256 profileId,
+        uint256 pubId,
+        uint256 budget,
+        address token,
+        uint256 rewardPerMirror,
+        uint256 minFollowers
+    ) external onlyOpenAction {
+        require(
+            IERC721(lensHubProxyContract).ownerOf(tokenId) ==
+                transactionExecutor,
+            "You are not the owner of this post"
+        );
 
-		IERC20(token).safeTransferFrom(msg.sender, address(this), budget);
+        IERC20(token).safeTransferFrom(
+            transactionExecutor,
+            address(this),
+            budget
+        );
 
-		promotions[profileId][pubId] = Promotion(
-			profileId,
-			pubId,
-			budget,
-			token,
-			rewardPerMirror,
-			minFollowers
-		);
+        promotions[profileId][pubId] = Promotion(
+            profileId,
+            pubId,
+            budget,
+            token,
+            rewardPerMirror,
+            minFollowers
+        );
+    }
+
+	function promote(
+		uint256 promotionProfileId,
+		uint256 promotionPubId,
+		uint256 promoterId
+	) external onlyOpenAction {
+		Promotion storage promotion = promotions[promotionProfileId][promotionPubId];
+		// TODO: add follower check
+
+		
 	}
 
-
-
-	function getPromotion(uint256 profileId, uint256 pubId)
-		external
-		view
-		returns (Promotion memory)
-	{
-		return promotions[profileId][pubId];
-	}
+    // Getters
+    function getPromotion(
+        uint256 profileId,
+        uint256 pubId
+    ) external view returns (Promotion memory) {
+        return promotions[profileId][pubId];
+    }
 }
